ddaa167f92794e677b05b5804a9e398411b348f7
yoav cohen <yoac@Post.bgu.ac.il>
2017-01-23 09:12:00 +0100

    youtube support

--- youtube.py
+++ youtube.py
@@ -9,7 +9,7 @@ from oauth2client.tools import argparser
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyB1upC6eXp2mWCiqA9tAof1DqtjCbTNacc"
+DEVELOPER_KEY = ""
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
